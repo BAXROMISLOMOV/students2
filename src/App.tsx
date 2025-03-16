deb9776efd3ace167f59559ebaf20967cc15b2e3
BAXROM <bahromislomov324@gmail.com>
2025-03-16 21:36:26 +0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ const StudentTable: React.FC = () => {
   ];
 
   return (
-    <div className="p-5 w-300  h-200">
+    <div className="p-5 ml-90 w-300   h-200">
       <Button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => setOpen(true)}>
         Add Student
       </Button>
